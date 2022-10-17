dc0e00e093f316f5712ab9b248b4b5ac48b69498
vicic8484 <victor.berhault@gmail.com>
2022-10-17 10:34:13 +0200

    Removed useless library

--- TP3/TP3.java
+++ TP3/TP3.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 
 public class TP3 {
     public static void main(String[] args){
